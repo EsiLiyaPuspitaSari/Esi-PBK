cc0c6abdde7ad963fc8a8fac4b257e18f1b439da
EsiLiyaPuspitaSari <esiliyapuspitasari@student.uir.ac.id>
2023-05-12 10:05:15 +0700

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <h1>Esi liya Puspita Sari</h1>
+    <h1>213510448</h1>
+    <h1>Teknik Informatika</h1>
+    <h1>Universitas Islam Riau</h1>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,119 @@
+<template>
+  <h1>Skincare Rekomandasi Esi</h1>
+  <div>
+    <input v-model="newTodo" placeholder="Masukkan Kegiatan">
+    <button @click="addTodo">Tambah Kegiatan</button>
+    <br><br>
+    <button @click="hideCompleted = !hideCompleted">{{ hideCompleted ? 'Tampilkan Semua' : 'Sembunyikan yang sudah selesai' }}</button>
+    <ul>
+      <li v-for="todo in visibleTodos" :key="todo.id">
+        <span :class="{ 'completed': todo.completed }" @click="toggleComplete(todo)">{{ todo.text }}</span>
+        <button v-if="!todo.completed" @click="completeTodo(todo.id)">Selesai</button>
+        <button v-if="todo.completed" @click="unCompleteTodo(todo.id)">Belum Selesai</button>
+        <button @click="removeTodo(todo.id)">Hapus</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      todos: [],
+      newTodo: '',
+      nextId: 1,
+      hideCompleted: false
+    }
+  },
+  computed: {
+    visibleTodos() {
+      if (this.hideCompleted) {
+        return this.todos.filter(todo => !todo.completed);
+      } else {
+        return this.todos;
+      }
+    }
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo.trim() !== '') {
+        this.todos.push({
+          id: this.nextId++,
+          text: this.newTodo,
+          completed: false
+        });
+        this.newTodo = '';
+      }
+    },
+    removeTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    },
+    completeTodo(id) {
+      const todo = this.todos.find(todo => todo.id === id);
+      if (todo) {
+        todo.completed = true;
+      }
+    },
+    unCompleteTodo(id) {
+      const todo = this.todos.find(todo => todo.id === id);
+      if (todo) {
+        todo.completed = false;
+      }
+    },
+    toggleComplete(todo) {
+      todo.completed = !todo.completed;
+    }
+  }
+}
+</script>
+<style>
+body {
+  background-color: palegreen; /* Ganti dengan warna yang Anda inginkan */
+}
+ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+li {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 10px;
+  padding: 10px;
+  border: 1px solid #eee;
+  border-radius: 10px;
+  background-color: #f8f8f8;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+  transition: all 0.3s ease;
+}
+
+li:hover {
+  transform: translateY(-3px);
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
+}
+
+.completed {
+  text-decoration: line-through;
+  color: #ccc;
+}
+
+button {
+  margin-left: 10px;
+  border: none;
+  border-radius: 5px;
+  background-color: #eee;
+  color: #444;
+  font-weight: bold;
+  padding: 6px 10px;
+  transition: all 0.3s ease;
+}
+
+button:hover {
+  background-color: #444;
+  color: #fff;
+}
+</style>
